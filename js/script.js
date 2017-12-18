c12a3e5e25304f703bc9aa6b1c0e05e6c3a782ba
g0-0py <vyseaurion@gmail.com>
2017-12-18 12:52:23 -0600

    ~
    changed variable name to newPixel to be more descriptive

--- js/script.js
+++ js/script.js
@@ -3,23 +3,22 @@ function createGrid(gridSize) {
   const container = document.getElementById('grid-container');
   
   //logic to set grid element size
-  pixelWidth = (600/gridSize-3).toString() + "px"
-  pixelHeight = (600/gridSize-3).toString() + "px"
+  pixelSize = (600/gridSize-3).toString() + "px"
 
   for(let i = gridSize**2; i > 0; i--) {
-    newDiv(pixelWidth, pixelHeight)
+    newPixel(pixelSize)
   }
 
-  function newDiv(pixelWidth, pixelHeight) {
-    var newDiv = document.createElement('div')
-    newDiv.setAttribute('id', divNumber)
-    newDiv.setAttribute('class', "divGrid")
-    newDiv.style.width = pixelWidth;
-    newDiv.style.height = pixelHeight;
-    container.appendChild(newDiv)
+  function newPixel(pixelSize) {
+    var newPixel = document.createElement('div')
+    newPixel.setAttribute('id', divNumber)
+    newPixel.setAttribute('class', "divGrid")
+    newPixel.style.height = pixelSize
+    newPixel.style.width = pixelSize
+    container.appendChild(newPixel)
     divNumber++
 
-    newDiv.addEventListener('mouseover', function(event) {
+    newPixel.addEventListener('mouseover', function(event) {
       event.target.style.background = 'black';
     });
   }
@@ -29,6 +28,6 @@ function destroyGrid() {
   const container = document.getElementById('grid-container');
 
   while(container.firstchild != 'null') {
-  container.removeChild(container.firstChild);
+    container.removeChild(container.firstChild);
   }
 }
