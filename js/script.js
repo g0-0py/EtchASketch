dd238083021f503d671b0900667e58d1192cfb8a
g0-0py <vyseaurion@gmail.com>
2017-12-17 22:53:47 -0600

    end of night commit
    didn't solve much. Added overflow:hidden to container, which is a sloppy
    and gross way of hiding things.

--- js/script.js
+++ js/script.js
@@ -2,20 +2,33 @@ function createGrid(gridSize) {
   let divNumber = 0;
   const container = document.getElementById('grid-container');
   
-  // add logic to set grid element size (w/h)
+  //logic to set grid element size
+  pixelWidth = (600/gridSize-3).toString() + "px"
+  pixelHeight = (600/gridSize-3).toString() + "px"
 
-  for (let i = gridSize ** 2; i > 0; i--) {
-    newDiv();
+  for(let i = gridSize**2; i > 0; i--) {
+    newDiv(pixelWidth, pixelHeight)
   }
-  
 
-  function newDiv() {
+  function newDiv(pixelWidth, pixelHeight) {
     var newDiv = document.createElement('div')
     newDiv.setAttribute('id', divNumber)
     newDiv.setAttribute('class', "divGrid")
-    newDiv.setAttribute('width' pixelWidth);
-    newDiv.setAttribute('height' pixelHeight)
+    newDiv.style.width = pixelWidth;
+    newDiv.style.height = pixelHeight;
     container.appendChild(newDiv)
     divNumber++
+
+    newDiv.addEventListener('mouseover', function(event) {
+      event.target.style.background = 'black';
+    });
+  }
+}
+
+function destroyGrid() {
+  const container = document.getElementById('grid-container');
+
+  while(container.firstchild != 'null') {
+  container.removeChild(container.firstChild);
   }
 }
