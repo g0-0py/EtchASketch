deda0f4462a28612c788a2bd825714fbc219eabe
g0-0py <vyseaurion@gmail.com>
2017-12-18 16:22:07 -0600

    added user grid size selection
    had to rework some of the logic. createGrid doesn't accept any arguments
    now.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-this.onLoad(createGrid(16))
+this.onLoad(createGrid())
 
 const gridButton = document.getElementById('gridButton');
 
@@ -7,18 +7,17 @@ gridButton.addEventListener('mouseover', function(event) {
 });
 
 
-function createGrid(gridSize) {
+function createGrid() {
   let pixelNumber = 1;
   const container = document.getElementById('gridContainer');
+  const gridSize = (document.getElementById('sizeChoice').value)
+
   container.style.setProperty("--gridSize", gridSize);
 
   //This'll clear our grid if the user resets it
   if(container.innerHTML != ""){
     container.innerHTML = "";
   }
-  
-
-
   
   for (let i = gridSize ** 2; i > 0; i--) {
     newPixel()
